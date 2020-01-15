c1cda632df14dc8f4e86298853c00c59d3847698
Lee Hilton <lee.hilton@leezilla.net>
2020-01-15 09:01:29 -0800

    Issue #1 - Sanitized input in case a null value is passed
    This will be commonly caused by a new field without any value and no casting on the model property.

--- src/Helper.php
+++ src/Helper.php
@@ -7,8 +7,9 @@ use VanOns\Laraberg\Helpers\EmbedHelper;
 
 class Helper
 {
-    public function render(string $html, bool $wrapHtml = true)
+    public function render(?string $html = '', bool $wrapHtml = true)
     {
+        $html = $html ?? '';
         $rendered = $wrapHtml ? '<div class="gutenberg__content wp-embed-responsive">' : '';
         $rendered .= BlockHelper::renderBlocks(EmbedHelper::renderEmbeds($html));
         $rendered .= $wrapHtml ? '</div>' : '';
